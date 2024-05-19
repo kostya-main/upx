bdd5ef72e44779ed602544ca5db8a2fce15265c4
kostya-main <simkin-kostya@mail.ru>
2024-05-20 01:50:40 +0300

    Transition to ESM

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-'use strict'
-module.exports = require('./lib')
+import * as fs from 'fs'
+import * as upxBin from './lib/upxBin.js'
+import * as opts2Args from './lib/opts2Args.js'
+
+
+class Upx {
+    constructor (file, opts) {
+      fileExists(file)
+      this.file = file
+      this.opts = opts || {}
+    }
+  
+    output (outPath) {
+      this._output = outPath
+      return this
+    }
+  
+    start () {
+      let self = this
+      return new Promise(function (resolve, reject) {
+        upxBin.default.call(self, opts2Args.default(self.opts), function (err, stats) {
+          if (err) return reject(err)
+          resolve(stats)
+        })
+      })
+    }
+}
+  
+export default function (file, opts) {
+    return new Upx(file, opts)
+}
+  
+function fileExists (file) {
+    try {
+      fs.statSync(file)
+    } catch (e) {
+      throw Error('Make sure the file exists: ' + file + ' ERROR ' + e)
+    }
+}
